12ffa06b32f74f1cc269f16836c9a8d0e4dd1517
berkutta <bmarty@kochag.ch>
2015-09-30 15:31:18 +0200

    Added AT Project for Energy Management

---
+++ ENERGY_MANAGEMENT_V0_1/ENERGY_MANAGEMENT_V0_1/ENERGY_MANAGEMENT_V0_1/ENERGY_MANAGEMENT_V0_1.cpp
@@ -0,0 +1,17 @@
+/*
+ * ENERGY_MANAGEMENT_V0_1.cpp
+ *
+ * Created: 30.09.2015 15:29:55
+ *  Author: bmarty
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ ENERGY_MANAGEMENT_V0_1/ENERGY_MANAGEMENT_V0_1/main.c
@@ -0,0 +1,21 @@
+/**
+* \file     main.c
+* \ingroup  g_applspec
+* \brief    Main file for Energy Management.
+* \author   Benjamin Marty
+*
+* This file is the place where the basic structure of the application/module is defined.
+*/
+
+
+#define F_CPU 8000000UL                                             //Set Clock for delay to 8MHz
+#include "util/delay.h"                                             //Delay Function
+
+int main(void)
+{
+
+    while(1)
+    {        
+
+    }
+}
