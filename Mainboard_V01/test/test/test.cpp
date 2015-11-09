d05f2008df6379aa52cbbf3c0e55ec10ca0f7f9e
Suter Eric <SuterE01@BircherReglomat>
2015-11-09 12:59:25 +0100

    TWI implementation, umstruktorierung und kommentiert und verbessert

--- Mainboard_V01/test/test/test.cpp
+++ Mainboard_V01/test/test/test.cpp
@@ -1,93 +1,238 @@
-/*
- * GccApplication1.cpp
- *
- * Created: 12.10.2015 19:15:21
- *  Author: Eric, Severin
- */ 
-
+//////////////////////////////////
+// Programm: Mainboard			//
+// Name:	 Eric Suter			//
+// Datum:	 8.11.2015			//
+// Version:	 1.1				//
+//////////////////////////////////
 
 #include <avr/io.h>
 #include <util/delay.h>
-
+#include <stdint.h>
 
 #define w_register(x) (0b00100000 | x)
 #define r_register(x) (0b00000000 | x)
 #define nop 0xFF
-#define ce XX
-#define eric severin
 
 #define DDR_SPI DDRB
 #define DD_SCK	DDRB1
 #define DD_MOSI DDRB2
 
+#define EM 0xFF
+#define MM 0x0F
 
- int received_data = 0;
+char r_data1 = 0;
+char r_data2 = 0;
+char r_data3 = 0;
+char r_data4 = 0;
+char r_data5 = 0;
+uint32_t motordata = 0;
+int m_data1 = 0;
+int m_data2 = 0;
+int transmit = 0;
+
+void init_radio(void);
+void receive_mode(void);
+uint32_t receive_data(void);
+int SPI_tranceive(int transmit);
+void transmit_mode(char mode, int data);
+void twi_init(void);
+void twi_transmit(char adress, char mode, int data);
+int twi_receive(char r_adress, char r_mode);
+void decode(void);
+
+int main (void){
+	
+	DDRF = 0xFF;													// INIT	von Ports
+	PORTF = 0x01;
+	
+	init_radio();													// Funkmodul und TWI initialisieren
+	twi_init();
+	
+	while(1){
+		
+		decode();													// Daten von Funkmodul laden und verarbeiten
+		
+		if(transmit == 1){											// Wenn daten von fernbedienung verlangt werden
+			
+			int ldata = twi_receive(EM, 0x00);						// Akkustand lesen
+			transmit_mode(0xFF, ldata);								// Akkustand übertragen
+			transmit_mode(0x0F, PORTC);								// Beleuchtungsstand übertragen
+			
+			transmit = 0;											// in den Empfangsmodus wechseln
+			receive_mode();
+			
+		}
+		
+		twi_transmit(MM, 0x01,m_data1);								// Daten für Servo senden
+		twi_transmit(MM, 0x02,m_data2);								// Daten für Motor senden
+
+	}
+}
+
+void decode(void){
+	
+	motordata = receive_data();										// daten lesen
+	m_data1 = (motordata >> 8) & 0x00FF;							// entschlüsseln von daten
+	m_data2 = motordata >> 24;										// entschlüsseln von daten
+	
+}
+
+//////////////////////////////////////////
+// Funktion: TWI Initialisieren
+// Name:	 Eric Suter
+// Datum:	 8.11.2015
+// Version:	 1.1
+//////////////////////////////////////////
+
+void twi_init(void){
+																
+																// Init von TWI EF
+	twi_transmit(MM, 0x01,0x0000);								// Init von Motor
+	twi_transmit(MM, 0x02,0x0000);								// Init von Motor
+	twi_receive(EM, 0x01);										// Akkustand laden
+	
+}
+
+//////////////////////////////////////////
+// Funktion: TWI Daten senden
+// Name:	 Eric Suter
+// Datum:	 8.11.2015
+// Version:	 1.1
+//////////////////////////////////////////
+
+void twi_transmit(char adress, char mode, int data){
+	
+																// EF
+	
+}
+
+//////////////////////////////////////////
+// Funktion: TWI Daten anfordern
+// Name:	 Eric Suter
+// Datum:	 8.11.2015
+// Version:	 1.1
+//////////////////////////////////////////
+
+int twi_receive(char r_adress, char r_mode){
+	
+	int accu = 0;											// define
+	
+															// daten empfangen EF
+	
+	return(accu);											// Wert zurückgeben
+}
+
+//////////////////////////////////////////
+// Funktion: Initialisiert das Funkmodul und SPI Schnittstelle.
+// Name:	 Eric Suter, Severin Landolt		
+// Datum:	 8.11.2015			
+// Version:	 1.1				
+//////////////////////////////////////////
 
 void init_radio(void)	{
 
- _delay_ms(120);									// POR Delay
- 
- DDR_SPI = (1 << DD_MOSI)|(1 << DD_SCK);			// Set MOSI and SCK output, all others input 
- SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);  				// Enable SPI, Master, set clock rate fck/16
- 
-SPI_tranceive(w_register(0b00000000));
-SPI_tranceive(0b0110010);							// Letztes Bit bestimmt RX 1 / 0 TX Mode
-_delay_us(1500);
- 
- }
- 
-
-
-int SPI_tranceive(int transmit){					// Sendet Daten per Spi
+	_delay_ms(120);										// POR Delay
 	
-	 /* Start transmission */
-	 SPDR = transmit;
-	 /* Wait for transmission complete */
-	 while(!(SPSR & (1<<SPIF)));
-	 
-	 return SPDR;									// Empfangene Daten geben
+	DDR_SPI = (1 << DD_MOSI)|(1 << DD_SCK);				// SPI Initialisieren
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);  				// SPI Initialisieren
+	
+	SPI_tranceive(w_register(0b00000000));
+	
+	SPI_tranceive(0b0110010);							// Letztes Bit bestimmt RX 1 / 0 TX Mode
+	
+	_delay_us(1500);
+	
+	receive_mode();
 	
 }
 
+//////////////////////////////////////////
+// Funktion: Übertägt SPI Daten und gibt empfangene Daten zurück.
+// Name:	 Eric Suter, Severin Landolt
+// Datum:	 8.11.2015
+// Version:	 1.1
+//////////////////////////////////////////
 
+int SPI_tranceive(int transmit){						// Sendet Daten per Spi
 	
-int receive_mode(void){
+	SPDR = transmit;									// Starte übertragung
+	
+	while(!(SPSR & (1<<SPIF)));							// Warten bis übertragung beendet ist
+	
+	return SPDR;										// Empfangene Daten geben
+	
+}
 
-SPI_tranceive(w_register(0b00000000));
-SPI_tranceive(0b0110011);							// Letztes Bit bestimmt RX 1 / 0 TX Mode
-ce = 1;
+//////////////////////////////////////////
+// Funktion: Setzt das Funkmodul in den Empfangs modus.
+// Name:	 Eric Suter, Severin Landolt
+// Datum:	 8.11.2015
+// Version:	 1.1
+//////////////////////////////////////////
 
-}	
+void receive_mode(void){
 
-
-int receive_data(void){
-
-SPI_tranceive(0b01100001);							// In den lesemodus wechseln	Muss vl. mit receive_mode(void); ersetzt werden
-
-SPI_tranceive(0xFF);								// Erstes Byte auslesen
-
-received_data = SPDR;
-
-for(int i = 31; i == 0; --i){						// Dummy Daten empfangen
-
-SPI_tranceive(0xFF);
-
-if ( SPDR != 0xFF ){ return(0); }
+	SPI_tranceive(w_register(0b00000000));
+	SPI_tranceive(0b0110011);							// Letztes Bit bestimmt RX 1 / 0 TX Mode
+	//ce = 1;
 
 }
 
+//////////////////////////////////////////
+// Funktion: Liest empfangene daten aus Funkmodul.
+// Name:	 Eric Suter, Severin Landolt
+// Datum:	 8.11.2015
+// Version:	 1.1
+//////////////////////////////////////////
 
+uint32_t receive_data(void){
+	
+	SPI_tranceive(0b01100001);							// In den lesemodus wechseln	Muss vl. mit receive_mode(void); ersetzt werden
+	
+	SPI_tranceive(0xFF);								// Erstes Byte auslesen
+	r_data1 = SPDR;
+	
+	if (r_data1 == 0x77){ transmit = 1; }				// Daten senden wechsel
+	
+	SPI_tranceive(0xFF);								// Zweites Byte auslesen
+	r_data2 = SPDR;
+	
+	SPI_tranceive(0xFF);								// Drites Byte auslesen
+	r_data3 = SPDR;
+	
+	SPI_tranceive(0xFF);								// Viertes Byte auslesen
+	r_data4 = SPDR;
+		
+	SPI_tranceive(0xFF);								// Fünftes Byte auslesen
+	r_data5 = SPDR;
+	
+	uint32_t result = ((((((((r_data5<<8)+r_data4)<<8)+r_data3)<<8)+r_data2)<<8)+r_data1);
+	
+	for(int i = 29; i == 0; --i){						// Dummy Daten empfangen
 
+		SPI_tranceive(0xFF);
+
+	}
+	
+	return(result);
+	
 }
 
+//////////////////////////////////////////
+// Funktion: Setzt das Funkmodul in den Übertragungsmodus und sendet die daten
+// Name:	 Eric Suter, Severin Landolt
+// Datum:	 8.11.2015
+// Version:	 1.1
+//////////////////////////////////////////
 
-int transmit_mode(char send_data){
+void transmit_mode(char mode, int data){
 
 	SPI_tranceive(0b10100000);							// FIFO Beschreiben
 
-	SPI_tranceive(send_data);							// Daten zum Senden geben
-
-	for(int i = 31; i == 0; --i){						// Dummy Daten senden
+	SPI_tranceive(mode);								// Daten zum Senden geben
+	SPI_tranceive(data | 0x00FF);
+	SPI_tranceive((data | 0xFF00) >> 8);
+	for(int i = 29; i == 0; --i){						// Dummy Daten senden
 
 		SPI_tranceive(0xFF);
 
@@ -95,22 +240,6 @@ int transmit_mode(char send_data){
 
 	SPI_tranceive(w_register(0b00000000));
 	SPI_tranceive(0b0110010);							// Letztes Bit bestimmt RX 1 / 0 TX Mode
-	ce = 1;
+	//ce = 1;
 
-}
-	
-
-
-
-
-int main (void){
-
-	while(1){
-
-
-		if (funktioniert);
-
-
-
-	}
 }
