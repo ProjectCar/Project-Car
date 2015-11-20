2c3872bee2006e0e5ff9f4e983ac8d1616b34b88
Berkutta <benjamin.marty@gmail.com>
2015-11-20 11:52:11 +0100

    Added basic ADC Functions

--- ENERGY_MANAGEMENT_V0_1/ENERGY_MANAGEMENT_V0_1/ENERGY_MANAGEMENT_V0_1/main.c
+++ ENERGY_MANAGEMENT_V0_1/ENERGY_MANAGEMENT_V0_1/ENERGY_MANAGEMENT_V0_1/main.c
@@ -19,3 +19,23 @@ int main(void)
 
     }
 }
+
+char adc_init(char channel)
+{
+	ADMUX = 0x00;				//Reset ADMUX register to 0
+	ADMUX = (1<<REFS0);			//Set ref to AVCC
+	ADMUX |= channel >> 4;		//Set channel
+	
+	ADCSRA = ( (1<<ADEN) | (1<<ADSC) | (1<<ADPS1) | (1<<ADPS0) );	//Enable ADC, Star ADC, Set Clock Prescaler to 8
+	
+	return 1;
+}
+
+int adc_sample()
+{
+	int result = 0;					//Setup space for ADC result
+	
+	result = ADCL + (ADCH >> 2);	//Get 10bit result from ADC registers. Keep in mind to read ADCH!
+	
+	return result;
+}
